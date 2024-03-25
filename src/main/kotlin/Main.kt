f8c49a1cebb9f1898516858f039cda472b0fbf28
shallo-nairat <shallonairat@gmail.com>
2024-03-25 08:28:29 +0300

    added new files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+fun main() {
+    Addition(20,30,50)
+
+    sentence("Barnie bakes brown bagels  and buns")
+    volume(2.5)
+    isPalindrome("madam")
+}
+
+
+fun Addition (number: Array<Int>): Array<Int> {
+
+   val sum = arrayOf(number.sum())
+   return sum
+   }
+
+
+fun sentence(word:String){
+  val extractedWord = sentence(word.slice(l))
+  println(extractedWord)
+
+
+}
+
+fun volume(radius:Double){
+    val volume = 4/3 *3.14159*(radius*3)
+    println(volume)
+}
+fun isPalindrome(word: String): String {
+   if (word==word.reversed(
+       println(True)
+   )
+   else(
+       println(False)
+   )
+    return
+
+}
